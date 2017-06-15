a32520f70338582ebdb81b612af970e3657b5c69
Thomas Nelson <tn90ca@gmail.com>
2017-06-15 11:52:36 -0400

    [EDIT] Fixed Self Looping Comments
    TeddyBot was found to create comment loops, it would keep commenting on its own comments. I have created a filter for the teddyRbot username so it no longer responds to itself.

--- teddybot.py
+++ teddybot.py
@@ -10,20 +10,21 @@ comment_log_file = "teddys_reply_log.csv"
 def save_reply(reply):
     with open(comment_log_file, 'a') as f:
         writer = csv.writer(f)
-        writer.writeline(reply)
+        writer.writerow(reply)
 # end def save_reply
 
 for comment in subreddit.stream.comments():
-    for name in ['Theodore Roosevelt', 'theodore roosevelt', 'Teddy Roosevelt', 'teddy roosevelt',
-                 'Teddy', 'teddy', 'theodore', 'roosevelt', 'rough riders', 'Theodore', 'Roosevelt',
-                 'Rough Riders']:
-        if name in comment.body:
-            teddy_reply = "Did someone say " + name + "?    http://i.imgur.com/XVeG35Z.jpg"
-            try:
-                comment.reply(teddy_reply)
-                reply = [datetime.datetime.now(), comment.id, name, comment.subreddit]
-                save_reply(reply)
-                print(reply)
-            except:
-                print("Teddy couldn't do this one!")
-            break
+    if comment.author != "teddyRbot":
+        for name in ['Theodore Roosevelt', 'theodore roosevelt', 'Teddy Roosevelt', 'teddy roosevelt',
+                     'Teddy', 'teddy', 'theodore', 'roosevelt', 'rough riders', 'Theodore', 'Roosevelt',
+                     'Rough Riders']:
+            if name in comment.body:
+                teddy_reply = "Did someone say " + name + "?    http://i.imgur.com/XVeG35Z.jpg"
+                try:
+                    comment.reply(teddy_reply)
+                    reply = [str(datetime.datetime.now()), str(comment.id), str(name), str(comment.subreddit)]
+                    save_reply(reply)
+                    print(reply)
+                except:
+                    print("Teddy couldn't do this one!")
+                break
